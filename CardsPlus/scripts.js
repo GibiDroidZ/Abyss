b0b56b90a8224a57c57546811bc6090eba27b2e9
GibiDroidZ <gibs.teodosio@gmail.com>
2022-06-01 02:12:08 +0800

    Cleanup v4

--- CardsPlus/scripts.js
+++ CardsPlus/scripts.js
@@ -2,7 +2,9 @@
 var scrollToTop = document.getElementById("goToTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -19,30 +21,11 @@ function topFunction() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {});
-  });
-/*
-$(function() {
-  $("#generateImage").click(function() {
-
-    //html2canvas(document.getElementById('GenshinCard')).then(function(canvas) {
-  //    document.getElementById('cardExportImage').appendChild(canvas);
-    //});
-var container = document.body;
-    html2canvas(container,{allowTaint : true}).then(function(canvas) {
-                var link = document.createElement("a");
-                document.body.appendChild(link);
-                link.download = "html_image.png";
-                link.href = canvas.toDataURL("image/png");
-                link.target = '_blank';
-                link.click();
-              });
-  });
-});*/
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {});
+});
 
 function downloadimage() {
-  //var container = document.getElementById("image-wrap"); //specific element on page
   var container = document.getElementById("GenshinCard");; // full page
   html2canvas(container, {
     allowTaint: true
@@ -50,7 +33,7 @@ function downloadimage() {
 
     var link = document.createElement("a");
     document.body.appendChild(link);
-    link.download = "html_image.jpg";
+    link.download = "Abyss_CharacterCheckList.jpg";
     link.href = canvas.toDataURL();
     link.target = '_blank';
     link.click();
@@ -92,7 +75,6 @@ for (var i = 0; i < charList.length; i++) {
 
   }
 
-  //$('#charList').append('<div class="col l2 dePad"><div class="characters Star' + charList[i].rarity + '"><div class="' + charList[i].vision + 'Vision vision"></div><input type="checkbox" id="' + charList[i].label + '" name="' + charList[i].label + '" value="' +charList[i].label + '" class="reset-checkbox filter"><img src="Characters/' + charList[i].label + '.png" /></div></div>');
   $('#charList').append('<div class="col l2 dePad"><div class="characters Star' + charList[i].rarity + '"><div class="' + charList[i].vision + 'Vision vision"></div><input type="checkbox" id="' + charList[i].label + '" name="' + charList[i].label + '" value="' + charList[i].label + '" class="reset-checkbox filter"><div class="charPic charPic-' + charList[i].label + '"></div></div></div>');
 }
 
@@ -100,13 +82,10 @@ var newBG;
 
 $('.characters input').click(function() {
   if ($(this).is(':checked')) {
-    //$('#CharacterInfo').append('');
     newBG = $(this).next().css('background-image').replace('/Gray', '');
     $(this).next().css('background-image', newBG);
     $(this).parent().addClass('hover');
-  }
-
-  else {
+  } else {
     newBG = $(this).next().css('background-image').replace('/Characters', '/Characters/Gray');
     $(this).next().css('background-image', newBG);
     $(this).parent().removeClass('hover');
